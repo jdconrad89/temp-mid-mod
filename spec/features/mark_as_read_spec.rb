afc9950cf11b11e9b50c41d0d1b93d377b2493dd
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-04-17 14:41:04 -0600

    update the mark as read test, the first portion is passing but the second is not yet as you need to refresh the page for the status to change

--- spec/features/mark_as_read_spec.rb
+++ spec/features/mark_as_read_spec.rb
@@ -1,16 +1,26 @@
 require "rails_helper"
 
 RSpec.describe "can mark links as read", :js => :true do
+  before do
+    User.create(email:'jason@conrad.com', password: 'password' )
+    visit "/login"
+    fill_in "email", with: 'jason@conrad.com'
+    fill_in "password", with: 'password'
+    click_on ("Submit")
+    visit "/links"
+    fill_in :url, with: "http://www.google.com"
+    fill_in :title, with: "Google Ninja"
+    click_on "Submit"
+  end
   scenario "Mark a link as read" do
-    Link.create(url:"https://turing.io", title:"Turing")
-    visit "/"
-    within('.link .read-status') do
+    visit '/'
+    within(".card") do
       expect(page).to have_text("false")
     end
 
     click_on "Mark as Read"
-
-    within('.link .read-status') do
+    visit '/'
+    within(".card") do
       expect(page).to have_text("true")
     end
 
