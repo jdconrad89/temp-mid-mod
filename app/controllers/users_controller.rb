65692a9194277fcbef8a37b7070e692868c72f73
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-04-17 11:35:15 -0600

    pull out unused methods from the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,18 +10,10 @@ class UsersController < ApplicationController
       redirect_to '/'
       flash[:notice] = 'You have successfully signed up!'
     else
-        flash[:notice] = "Uh oh, your passwords don't match!" if passwords_do_not_match?
-        flash[:notice] = "Uh oh, you forgot to enter a password!" if password_missing?
-        flash[:notice] = "Uh oh, you forgot to enter the password confirmation!" if password_confirmation_missing?
-        flash[:notice] = "Uh oh, you forgot to enter a password and password confirmation!" if both_passwords_missing?
-        flash[:notice] = "Uh oh, you forgot to enter an email address!" if email_missing?
-        flash[:notice] = "Uh oh, you didn't enter any information!" if everything_missing?
+      error_messages
       redirect_to '/signup'
     end
   end
-
-
-
 
   private
 
@@ -29,27 +21,12 @@ class UsersController < ApplicationController
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
-  def password_missing?
-    params[:user][:password] == ''
-  end
-
-  def password_confirmation_missing?
-    params[:user][:password_confirmation] == ''
-  end
-
-  def passwords_do_not_match?
-    params[:user][:password] != params[:user][:password_confirmation]
-  end
-
-  def email_missing?
-    params[:user][:email] == ''
-  end
-
-  def both_passwords_missing?
-    password_missing? && password_confirmation_missing?
-  end
-
-  def everything_missing?
-    both_passwords_missing? && email_missing?
+  def error_messages
+    flash[:notice] = "Uh oh, your passwords don't match!" if User.passwords_do_not_match?(params)
+    flash[:notice] = "Uh oh, you forgot to enter a password!" if User.password_missing?(params)
+    flash[:notice] = "Uh oh, you forgot to enter the password confirmation!" if User.password_confirmation_missing?(params)
+    flash[:notice] = "Uh oh, you forgot to enter a password and password confirmation!" if User.both_passwords_missing?(params)
+    flash[:notice] = "Uh oh, you forgot to enter an email address!" if User.email_missing?(params)
+    flash[:notice] = "Uh oh, you didn't enter any information!" if User.everything_missing?(params)
   end
 end
