9fa6302be21ef990ee8e0518203d29ec70b98cb3
Jason Conrad <jason.d.conrad.89@gmail.com>
2017-04-18 15:29:35 -0600

    add helper methods for finding out if a read is a top read or hot read

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
   def read?(link)
     link.read == true
   end
+
+  def top_read?(link)
+    hot_reads = Link.find_hot_reads
+    link["url"] == hot_reads.first["url"]
+  end
+
+  def hot_read?(link)
+    hot_reads = Link.find_hot_reads
+    urls = []
+    hot_reads[1..-1].each do |hot_read|
+      urls << hot_read["url"]
+    end
+    urls.include?(link["url"])
+  end
 end
